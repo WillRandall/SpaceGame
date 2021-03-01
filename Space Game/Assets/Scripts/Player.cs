c7058250d0d55b3400fe439b74968418c06c2d74
Alejandra Gomez <21.alejandra.gomez@ksd.org>
2021-03-01 10:05:44 -0800

    adding test scene

---
+++ Space Game/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //congfig
+    [SerializeField] float walkSpeed = 5f;
+    //Status
+    bool isAlive = true;
+    //Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
+    float gravityScaleAtStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<myAnimator>();
+        myBodyCollider = GetComponent<CapuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive) { return; }
+
+        Walk();
+    }
+
+    private void Walk()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * walkSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Walking", playerHasHorizontalSpeed);
+    }
+}
